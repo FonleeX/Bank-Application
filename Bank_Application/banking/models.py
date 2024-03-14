7b07fe41f835132f2400c77585c4508d98340cba
FonleeX <filipobajtek1@gmail.com>
2024-03-14 20:30:58 +0000

    Added admin superuser
    added a admin user to manage databases

--- Bank_Application/banking/models.py
+++ Bank_Application/banking/models.py
@@ -5,14 +5,15 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, null=True,)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    #DOB
-    #country of birth
-    #city
-    #street name & number
+    date_of_birth = models.DateField(null=True)
+    city = models.CharField(max_length = 64, defualt="Lincoln")
+    address = models.TextField(max_length=200, defualt="Brayford Pool")
+    postcode = models.CharField(max_length=8, )
     #postcode
-    address = models.TextField(blank=True, null=True)
+    
+
+    email = models.EmailField(unique=True, max_length = 254)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- Bank_Application/banking/views.py
+++ Bank_Application/banking/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'banking/home.html')
 
 def redirect_to_home(request):
     return redirect('home')
+
+def register(request):
+    return render(request, 'banking/register.html')
+
+def login(request):
+    return render(request, 'banking/login.html')
